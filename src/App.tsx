f19d58a43bc9e0ef7725d2812530656f412c455f
Roland Seres <roland.seres90@gmail.com>
2024-06-18 14:12:49 +0200

    First page in progress

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,80 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import { Store } from "./utils/tagStore";
 import { IndexStore } from "./utils/indexStore";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import AddIcon from "@mui/icons-material/Add";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [file, setFile] = useState<File | null>(null);
+  const [fileName, setFileName] = useState("");
 
-
-  const STAMP = 'bdf637f9d62784457b8d1e67c89fc5433c84cbee392290485f810444c95fd165';
-  const BEE_API_URL = 'http://localhost:1633';
-  const indexStore = new IndexStore('12341234'.repeat(8), STAMP, BEE_API_URL);
-  useEffect(() => { 
-    test();
-  });
-  async function test() {
-    const obj = { 'kutya': ['ref1', 'ref2', 'ref3'] };
-    console.log(await indexStore.initialize())
-    console.log(await indexStore.set(obj))
-    console.log(await indexStore.get())
-  }
-
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files ? event.target.files[0] : null;
+    if (file) {
+      setFile(file);
+      setFileName(file.name); // Itt tároljuk el a fájl nevét
+    }
+  };
 
   return (
     <>
-      <div>
-        <div>
-          <div>Select content</div>
+      <div className="main">
+        <div className="leftPanel">
+          <div className="contentSelectText">Select content</div>
+          <div className="subText">Add a file to upload on SWARM!</div>
+          <input
+            accept="image/*"
+            type="file"
+            id="contained-button-file"
+            onChange={handleFileChange}
+            style={{ display: "none" }}
+          />
+          <label htmlFor="contained-button-file">
+            <Button
+              variant="contained"
+              color="primary"
+              component="div"
+              style={{
+                backgroundColor: "white",
+                color: "#19002933",
+                fontSize: 16,
+                fontWeight: 400,
+                lineHeight: "32px",
+                justifyContent: "left",
+                width: "569px",
+                height: "48px",
+                marginTop: "40px",
+                marginLeft: "20px",
+                boxShadow: "none",
+              }}
+            >
+              Path and filename
+            </Button>
+          </label>
         </div>
+        <div className="divider"></div>
         <div>
-          <div>Select content</div>
+          <div className="indexingText">Indexing</div>
+          <div className="subText" style={{ color: "#19002933" }}>
+            Add keywords for indexing!
+          </div>
+          <Button
+            style={{
+              backgroundColor: "white",
+              color: "#19002933",
+              fontSize: 16,
+              fontWeight: 400,
+              lineHeight: "32px",
+              justifyContent: "center",
+              width: "72px",
+              height: "48px",
+              marginTop: "40px",
+              marginLeft: "20px",
+            }}
+          >
+            <AddIcon />
+          </Button>
         </div>
       </div>
     </>
